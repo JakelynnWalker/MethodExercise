9d7d057d7fcb52e8fd75f81ee1c1ac456390f0a4
Jakelynn Walker <jakelynnwalker15@icloud.com>
2021-04-05 15:07:30 -0500

    saving methodexercise

---
+++ repos/SelectionStatementsExercise/SelectionStatementsExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SelectionStatementsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("try to guess my favorite number");
+            int x = int.Parse(Console.ReadLine());
+            if (= 32)
+            {
+                Console.WriteLine("You are right!");
+            }
+            else if(x>32)
+            {
+                Console.WriteLine("Too low.");
+            }
+            else (x<32)
+            {
+                Console.WriteLine("Too high.");
+            }
+            Console.WriteLine("Hello World!");
+
+            string subject
+
+
+        }
+    }
+}
